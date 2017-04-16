5aa1918e421e9eb2f47687c82cfa886c273e4c17
陈博洋 <kuzoncby@gmail.com>
2017-04-16 13:54:45 +0800

    split chart to a single vue file

---
+++ resources/assets/js/components/power-chart.vue
@@ -0,0 +1,51 @@
+<template>
+    <canvas ref="power" class="center-block"></canvas>
+</template>
+
+<script>
+    import Chart from 'chart.js';
+    export default {
+        props: ['room'],
+        mounted() {
+            var url = '/v1/room/' + this.room;
+            this.$http.get(url).then((response) => {
+                var current = response.data.current_power;
+                var total = response.data.total_power;
+                var ctx = this.$refs.power;
+                this.renderChart(ctx, current, total);
+            });
+        },
+        methods: {
+            renderChart: function (ctx, current, total) {
+                var myChart = new Chart(ctx, {
+                    type: 'bar',
+                    data: {
+                        labels: ["此时", "总和"],
+                        datasets: [{
+                            label: '# 能量消耗',
+                            data: [current, total],
+                            backgroundColor: [
+                                'rgba(255, 99, 132, 0.2)',
+                                'rgba(54, 162, 235, 0.2)'
+                            ],
+                            borderColor: [
+                                'rgba(255,99,132,1)',
+                                'rgba(54, 162, 235, 1)'
+                            ],
+                            borderWidth: 1
+                        }]
+                    },
+                    options: {
+                        scales: {
+                            yAxes: [{
+                                ticks: {
+                                    beginAtZero: true
+                                }
+                            }]
+                        }
+                    }
+                });
+            }
+        }
+    }
+</script>
